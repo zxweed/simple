84b28f58ded51d75c54b4d074fbf42f94731c216
Andrey Lyashenko <a_lyashenko@mail.ru>
2022-05-20 10:18:29 +0300

    Added buy/sell field calculations while resampling

--- ohlc.py
+++ ohlc.py
@@ -11,6 +11,7 @@ def resampleVolume(T: np.array, threshold: int, OHLC: np.array) -> int:
 
     while t < len(T):
         open = high = low = T.PriceA[t]
+        buy = sell = 0
         while t < len(T) and volume < threshold:
             volume += np.abs(T.VolumeA[t])
             if T.PriceA[t] > high:
@@ -18,13 +19,19 @@ def resampleVolume(T: np.array, threshold: int, OHLC: np.array) -> int:
             if T.PriceA[t] < low:
                 low = T.PriceA[t]
             t += 1
+            if T.VolumeA[t] > 0:
+                buy += T.VolumeA[t]
+            else:
+                sell += T.VolumeA[t]
 
-        OHLC[c]['DT'] = T.DateTimeA[t]
-        OHLC[c]['Open'] = open
-        OHLC[c]['High'] = high
-        OHLC[c]['Low'] = low
-        OHLC[c]['Close'] = T.PriceA[t]
-        OHLC[c]['Volume'] = volume
+        OHLC['DT'][c] = T.DateTimeA[t]
+        OHLC['Open'][c] = open
+        OHLC['High'][c] = high
+        OHLC['Low'][c] = low
+        OHLC['Close'][c] = T.PriceA[t]
+        OHLC['Volume'][c] = volume
+        OHLC['Buy'][c] = buy
+        OHLC['Sell'][c] = sell
         c += 1
         volume = 0
 
