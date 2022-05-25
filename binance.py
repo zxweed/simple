b5556a6c133255ba53584a244cffb8e7161f8957
zxweed <zxweed@gmail.com>
2022-05-25 18:11:18 +0300

    added asyncio req

--- binance.py
+++ binance.py
@@ -12,14 +12,15 @@ async def asyncOHLC(tm, ticker, interval):
         async with session.get(url) as response:
             json = await response.json()
             df = pd.DataFrame(json)
-            df = df.set_index(df[0].astype('M8[ms]'))[[1, 2, 3, 4, 5]].apply(pd.to_numeric)
-            df.index.name = 'DateTime'
-            df.columns = ['Open', 'High', 'Low', 'Close', 'Volume']
-            return df
+            if len(df) > 0:
+                df = df.set_index(df[0].astype('M8[ms]'))[[1, 2, 3, 4, 5]].apply(pd.to_numeric)
+                df.index.name = 'DateTime'
+                df.columns = ['Open', 'High', 'Low', 'Close', 'Volume']
+                return df
 
 
-async def getOHLC(ticker, start_time, end_time, interval=10):
-    TM = pd.date_range(start_time, end_time, freq=f'{500*interval}min')
-    tasks = [asyncio.create_task(asyncOHLC(tm, ticker, f'{interval}m')) for tm in TM]
+async def getOHLC(ticker, start_time, end_time, minutes=5):
+    TM = pd.date_range(start_time, end_time, freq=f'{500*minutes}min')
+    tasks = [asyncio.create_task(asyncOHLC(tm, ticker, f'{minutes}m')) for tm in TM]
     results = await asyncio.gather(*tasks)
-    return pd.concat(results)#.sort_index()
+    return pd.concat(results).sort_index()
