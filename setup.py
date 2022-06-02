47b9b09ff2e672d9ee4efdf03e6f89012f7a238b
Andrey Lyashenko <a_lyashenko@mail.ru>
2022-06-02 22:55:20 +0300

    added setup.py at the root

--- setup.py
+++ setup.py
@@ -1,30 +1,15 @@
-import setuptools
+from setuptools import setup
 import os
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
 
-
-def read_requirements():
-    reqs_path = os.path.join(__location__, 'requirements.txt')
-    with open(reqs_path, encoding='utf8') as f:
-        reqs = [line.strip() for line in f if not line.strip().startswith('#')]
-
-    names = []
-    for req in reqs:
-        names.append(req)
-    return {'install_requires': names}
-
-
-setuptools.setup(
+setup(
     name="simple",
     version="0.4",
     author="zxweed",
     author_email="zxweed@gmail.com",
     description="A package that implements some simple trading functions",
-    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/zxweed/simple",
     packages=setuptools.find_packages(),
@@ -35,6 +20,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
-    **read_requirements()
+    python_requires='>=3.6'
 )
