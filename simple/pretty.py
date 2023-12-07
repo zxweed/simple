de031f5712695740d2ff442a78004dd365d6d840
landy0 <landy@example.com>
2023-12-07 16:14:58 +0300

    plotROC(), plotImportance() funcs added

--- simple/pretty.py
+++ simple/pretty.py
@@ -1,14 +1,21 @@
 # -*- coding: utf-8 -*-
-from tqdm.auto import tqdm
 import pandas as pd
 from pandas.io.formats.style import Styler
 import numpy as np
-from joblib import Parallel, delayed, cpu_count
+from numpy.typing import NDArray
+from itertools import product, zip_longest, cycle, product
+from math import ceil, sqrt
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import matplotlib.patheffects as pe
+import seaborn as sns
 import inspect
+from sklearn.metrics import confusion_matrix, roc_curve, auc
+from sklearn.preprocessing import label_binarize
+from joblib import Parallel, delayed, cpu_count
 from psutil import cpu_percent
 from datetime import datetime, timedelta
-import matplotlib as mpl
-from itertools import product
+from tqdm.auto import tqdm
 
 
 class tqdmParallel(Parallel):
@@ -69,58 +76,14 @@ def iterable(obj):
         return False
 
 
-def pxmap(func: callable, param, **kwargs):
-    """Another parallel map implementation, returns DataFrame, supports many parameters and results"""
-
-    param_names = inspect.getfullargspec(func).args
-    param_list = list(param)
-
-    with tqdmParallel(total=len(param_list),  **kwargs) as P:
-        FUNC = delayed(func)
-        X = list(P(FUNC(*tpl(param)) for param in param_list))
-
-    # append result names
-    if len(X) > 0:
-        if type(X[0]) == dict:
-            param_names.extend(X[0].keys())
-        elif type(X[0]) == tuple:
-            param_names.extend([f'result_{n}' for n in range(len(X[0]))])
-        else:
-            param_names.append('result')
-
-    return pd.DataFrame([(*tpl(x[0]), *tpl(x[1])) for x in zip(param_list, X)], columns=param_names)
-
-
-def prun(indicator: callable, src, period: int, threads: int = cpu_count(), progress: bool = True) -> np.array:
-    """Parallel calculate any indicator by slicing source series"""
-    page_size = (len(src) - period + (threads - 1)) // threads
-    start_indexes = [t * page_size for t in range(threads)]
-    result_indexes = list(range(period, len(src), page_size))
-
-    with tqdmParallel(total=len(start_indexes), n_jobs=threads, require='sharedmem', progress=progress) as P:
-        FUNC = delayed(indicator)
-        slice_size = period + page_size
-        X = P(FUNC(src[start:start + slice_size], period) for start in start_indexes)
-
-    # store all slices to the result series
-    result = np.zeros(len(src))
-    for page_start, x in zip(result_indexes, X):
-        m = page_start + page_size
-        page_stop = m if m < len(result) else len(result)
-        result[page_start:page_stop] = x[period:]
-
-    # fill prefix with first value
-    result[:period] = result[period]
-    return result
-
-
-def Corr(X, Y):
+def Corr(X: NDArray, Y: NDArray) -> float:
     """Correlation coefficient"""
     return np.corrcoef(np.nan_to_num(X), np.nan_to_num(Y))[0, 1] * 100
 
 
 def background(self, scale='Linear', cmap='RdYlGn', **css) -> pd.DataFrame:
-    """For use with `DataFrame.style.apply` this function will apply a heatmap color gradient *elementwise* to the calling DataFrame
+    """For use with `DataFrame.style.apply` this function will apply a heatmap
+       color gradient *elementwise* to the calling DataFrame
 
     self : pd.DataFrame
         The calling DataFrame. This argument is automatically passed in by the `DataFrame.style.apply` method
@@ -141,10 +104,11 @@ def background(self, scale='Linear', cmap='RdYlGn', **css) -> pd.DataFrame:
     q = np.percentile(cvals, 99.9)
     vmax = max(cvals.max(), abs(np.clip(cvals, -q, q).min()))
     if scale == 'Linear':
-        norm = mpl.colors.Normalize(vmin=-vmax, vmax=vmax, clip=True)  # Linear colorscale
+        # Linear colorscale
+        norm = mpl.colors.Normalize(vmin=-vmax, vmax=vmax, clip=True)
     else:
-        norm = mpl.colors.SymLogNorm(linthresh=1, vmin=-vmax, vmax=vmax, clip=True,
-                                     base=2.17)  # Logarithmic color scale
+        # Logarithmic color scale
+        norm = mpl.colors.SymLogNorm(linthresh=1, vmin=-vmax, vmax=vmax, clip=True, base=2.17)
     mapper = mpl.cm.ScalarMappable(norm=norm, cmap=cmap)
 
     styles = []
@@ -157,8 +121,8 @@ def background(self, scale='Linear', cmap='RdYlGn', **css) -> pd.DataFrame:
     return pd.DataFrame(styles, index=self.index, columns=self.columns)
 
 
-def hline(row, color='black', width='1px'):
-    return [f'border-bottom: {width} solid {color};' for _ in row]
+def hline(row, color='black', width='1px'): 
+    return [f"border-bottom: {width} solid {color};" for _ in row]
 
 
 def pp(X: pd.DataFrame, float_format: str = None, h_subset=None) -> Styler:
@@ -170,3 +134,198 @@ def pp(X: pd.DataFrame, float_format: str = None, h_subset=None) -> Styler:
         float_format = '{:,.2f}'
     x = X.style.format(float_format).apply(background, axis=None)
     return x if h_subset is None else x.apply(hline, axis=1, subset=h_subset)
+
+
+def get_sides(n: int) -> tuple:
+    """
+    Returns the width and height of a rectangle that contains n subplots.
+    
+    Parameters:
+    n (int): The number of subplots.
+    
+    Returns:
+    tuple: A tuple containing the width and height of the rectangle.
+    """
+    # Calculate the initial values for x and y
+    x = ceil(sqrt(n))
+    y = x
+
+    # Decrease y until the rectangle fits the number of subplots
+    while x * y >= n:
+        y -= 1
+
+    # Ensure the rectangle is not too small
+    y += 1
+
+    # Adjust x and y if the rectangle is too large
+    if (x - 1) * (y + 1) >= n:
+        x -= 1
+        y += 1
+
+    return x, y
+
+
+def plotHeatmaps(df: NDArray, x_name: str, y_name: str, value_name: str,
+                 z_name: str = None, g_name: str = None, 
+                 value_max: float = None,
+                 fig_width=16, text_color='blue', stroke: bool = False) -> plt.figure:
+    """
+    Create grid figure with heatmap subplots.
+    
+    Parameters:
+        df (NDArray or DataFrame): The input data.
+        x_name (str): The name of the column to be used as x-axis for each subplot.
+        y_name (str): The name of the column to be used as y-axis for each subplot.
+        value_name (str): The name of the column to be used as values.
+        z_name (str, optional): The name of the column to be used as z-value for grid.
+        g_name (str, optional): The name of the column to be used as g-value for grid.
+        value_max (float, optional): The maximum value for the color map.
+        fig_width (int, optional): The width of the figure.
+        text_color (str, optional): The color of the text.
+        stroke (bool, optional): Whether to add a stroke to the text.
+    
+    Returns:
+        plt.figure: The figure object.
+    """
+
+    Z = np.unique(df[z_name]) if z_name else None
+    G = np.unique(df[g_name]) if g_name else None
+    
+    # determine number of rows and columns
+    if not z_name and not g_name:
+        # single plot
+        rows, cols = 1, 1
+        one = df
+        param = [0]
+    elif z_name and g_name:
+        # subplot grid by z-value and g-value
+        rows, cols = len(Z), len(G)
+        one = df[(df[z_name] == Z[0]) & (df[g_name] == G[0])]
+        param = product(Z, G)
+    elif g_name and not z_name:
+        # one subplot per z-value (no grid, just rectangular sequence)
+        rows, cols = get_sides(len(G))
+        one = df[df[g_name] == G[0]]
+        param = G
+    elif z_name and not g_name:
+        # one subplot per g-value (no grid, just rectangular sequence)
+        rows, cols = get_sides(len(Z))
+        one = df[df[z_name] == Z[0]]
+        param = Z
+
+    # determine width and height of figure
+    pvt = one.pivot(columns=x_name, index=y_name, values=value_name).values
+    k1 = rows / cols
+    k2 = pvt.shape[0] / pvt.shape[1]
+    fig_height = fig_width * k1 * k2
+    fig, axs = plt.subplots(rows, cols, figsize=(fig_width, fig_height))
+    fig.subplots_adjust(wspace=0.05, hspace=0.05)
+
+    values = df[value_name]
+    h = value_max if value_max is not None else values[values > 0].max()
+    path_effects = [pe.Stroke(linewidth=2, foreground='black'), pe.Normal()] if stroke else None
+
+    # iterate over z/g-values (one value combination for each subplot)
+    for p, ax in [(param[0], axs)] if isinstance(axs, plt.Axes) else zip_longest(param, axs.flatten()):
+        # filter one slice for each subplot
+        if z_name and not g_name:
+            one = df[df[z_name] == p]
+            text = f'{z_name}={p}\n'
+        elif g_name and not z_name:
+            one = df[df[g_name] == p]
+            text = f'{g_name}={p}\n'
+        elif z_name and g_name:
+            one = df[(df[z_name] == p[0]) & (df[g_name] == p[1])]
+            text = f'{z_name}={p[0]}\n{g_name}={p[1]}\n'
+        else:
+            one = df
+            text = ''
+        pvt = one.pivot(columns=x_name, index=y_name, values=value_name).values
+
+        if len(pvt) > 0:
+            ax.imshow(pvt, cmap='RdYlGn', vmin=-h, vmax=h)
+            
+            # place text in the center of heatmap
+            y, x = [s/2 for s in pvt.shape]
+            text += f'\nmax({value_name})={pvt.max():,.1f}\nmean({value_name})={pvt.mean():,.1f}'
+            ax.text(x, y, text, color=text_color, ha='center', va='center', path_effects=path_effects)
+
+        # the number of subplots can be more than the data, but it is necessary to set parameters for them too
+        ax.tick_params(left=False, right=False, labelleft=False, labelbottom=False, bottom=False)
+
+    plt.close(fig)
+    return fig
+
+
+def plotImportance(model, names=None, top=20, palette='Blues_r', ax=None):
+    """
+    Plot the feature importance chart.
+
+    Args:
+        model: The machine learning model.
+        names: A list of feature names. If None, it will check if the model has a 'feature_names_' attribute.
+        top: The number of top features to display.
+        palette: The color palette for the barplot.
+        ax: The axis to plot the chart on. If None, a new axis will be created.
+    """
+    # Check if the model has a 'feature_names_' attribute and names is None
+    names = model.feature_names_ if hasattr(model, 'feature_names_') and names is None else names
+
+    # Check if the model has a 'feature_importances_' attribute
+    if hasattr(model, 'feature_importances_'):
+        # Sort the feature importances in descending order and select the top features
+        f = pd.Series(model.feature_importances_, index=names).sort_values(ascending=False)[:top]
+        sns.barplot(y=f.index, x=f, palette=palette, ax=ax).set(ylabel='Feature importance')
+    else:
+         print("Selected model does not support feature_importances_")
+
+
+def plotROC(model, X_test, y_test):
+    """
+    Generate the Receiver Operating Characteristic (ROC) curve for a multi-class classification model.
+    
+    Parameters:
+        model (object): The trained classification model.
+        X_test (array-like): The feature matrix of the test set.
+        y_test (array-like): The true labels of the test set.
+        
+    Returns:
+        fig (object): The matplotlib figure object containing the ROC curve and the confusion matrix.
+    """
+    y_test_binarized = label_binarize(y_test, classes=np.unique(y_test))
+
+    n_classes = y_test_binarized.shape[1]
+    fpr = dict()
+    tpr = dict()
+    roc_auc = dict()
+
+    y_pred, y_scores = model.predict(X_test), model.predict_proba(X_test)
+
+    for i in range(n_classes):
+        fpr[i], tpr[i], _ = roc_curve(y_test_binarized[:, i], y_scores[:, i])
+        roc_auc[i] = auc(fpr[i], tpr[i])
+
+    fig, ax = plt.subplots(1, 2, figsize=(14, 5))
+    colors = cycle(['blue', 'red', 'green', 'orange', 'cyan', 'magenta'])
+
+    # plot ROC chart
+    for i, color in zip(range(n_classes), colors):
+        label = 'ROC curve of class {0} (area = {1:0.2f})'.format(i, roc_auc[i])
+        ax[0].plot(fpr[i], tpr[i], color=color, lw=2, alpha=0.5, label=label)
+        
+    ax[0].plot([0, 1], [0, 1], color='black', lw=1, linestyle='--')
+    ax[0].set_xlabel('False Positive Rate')
+    ax[0].set_ylabel('True Positive Rate')
+    ax[0].set_title('Multi-class ROC')
+    ax[0].legend(loc="lower right")
+
+    # plot confusion matrix
+    conf_matrix = confusion_matrix(y_test, y_pred)
+    sns.heatmap(conf_matrix, annot=True, cmap='Blues', fmt="g", ax=ax[1], linewidths=3, linecolor='white');
+
+    ax[1].set_title('Confusion Matrix')
+    ax[1].set_xlabel('Predicted Label')
+    ax[1].set_ylabel('True Label')
+    
+    plt.close(fig)
+    return fig
